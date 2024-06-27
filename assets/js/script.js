f210ea214bf7558072236033f48fe133822dd2de
Carmen <carmencheney99@gmail.com>
2024-06-27 15:55:32 -0400

    LDs are the worst

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,38 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
-}
+    if (nextId == null) {
+            nextId = 1;
+        } else {
+            nextId = nextId + 1;
+        }
+        localStorage.setItem("nextId", nextId);
+    }
+    
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const carddiv = jQuery('<div>', {
+        class: 'card',
+        id: task.id
+    })
     const div = jQuery('<div>', {
-        id: task.id,
         class: 'card-body',
         title: task.title
     });
+    const today = new Date();
+    const deadline = new Date(task.deadline);
+    today.setHours(0,0,0,0);
+    deadline.setHours(0,0,0,0);
+    deadline.setDate(deadline.getDate()+1)
+    if(deadline.toDateString() === today.toDateString()) {
+        $(div).addClass('due')
+    } else if (deadline < today) {
+        $(div).addClass('overdue')
+    }
     jQuery('<h3>', {
         html: task.title
     }).appendTo(div);
@@ -24,6 +43,16 @@ function createTaskCard(task) {
         html: "Deadline: " + task.deadline
     }).appendTo(div);
     div.appendTo("#" + task.state + "-cards");
+    div.appendTo(carddiv)
+    carddiv.appendTo("#" + task.state + "-cards");
+
+    jQuery('<button>', {
+        html: 'X',
+        class: 'btn'
+    }).click(handleDeleteTask).appendTo(div);
+    div.appendTo(carddiv)
+    carddiv.appendTo("#" + task.state + "-cards");
+    
     
 }
 
@@ -39,25 +68,31 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-    const title= $("#title").val()
-    const description= $("#description").val()
-    const deadline= $("#deadline").val()
+    const title = $("#title").val();
+    const description = $("#description").val();
+    const deadline = $("#deadline").val();
 
-    const task= {
+generateTaskId();
+    const task = {
+        id: nextId,
         title: title,
         description: description,
         deadline: deadline,
-        state: "todo",
+        state: "todo"
     };
 
     taskList.push(task);
-    localStorage.setItem("tasks", JSON.stringify(tasksList))
+    localStorage.setItem("tasks", JSON.stringify(taskList))
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
-}
+    const divId = this.parentElement.parentElement.id;
+        const index = taskList.indexOf(taskList.find(x=>x.id==divId));
+        taskList.splice(index, 1);
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+        renderTaskList();
+    }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
@@ -66,10 +101,8 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    $("#newTask").submit(handleAddTask())
+    $("#newTask").submit(handleAddTask)
     renderTaskList()
     // add sumbit button listener
-    $("#newTask").submit(handleAddTask)
-
 }); 
 
